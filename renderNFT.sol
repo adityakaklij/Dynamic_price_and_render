b3408fb612741d473f452361ff8c28230634d320
adityakaklij <90379168+adityakaklij@users.noreply.github.com>
2022-03-09 12:49:54 +0530

    Update renderNFT.sol

--- renderNFT.sol
+++ renderNFT.sol
@@ -1,8 +1,5 @@
 // SPDX-License-Identifier: MIT
 
-// Developed by  Adi team
-
-
 // File: @openzeppelin/contracts/utils/introspection/IERC165.sol
 pragma solidity ^0.8.0;
 /**
@@ -1228,7 +1225,7 @@ contract DynamicNftTest is ERC721Enumerable, Ownable {
   string baseURI;
   string public baseExtension = ".json";
   uint256 public cost = 10 ether;
-  uint256 public maxSupply = 10;
+  uint256 public maxSupply = 100;
   uint256 public headStart = block.timestamp + 1 days;
   bool public paused = false;
   bool public revealed = false;
@@ -1258,180 +1255,22 @@ function dynamicPrice(uint256 _supply) public pure returns (uint256 _cost){
     if ((_supply >= 0) &&  (_supply <10)){
         return 10 ether;
     }
-    else if ((_supply >= 10) &&  (_supply <50)){
+    else if ((_supply >= 10) &&  (_supply <30)){
         return 25 ether;
     }
-    else if ((_supply >= 50) &&  (_supply <75)){
-        return 30 ether;
+    else if ((_supply >= 30) &&  (_supply <50)){
+        return 35 ether;
     }
-    else if ((_supply >= 75) &&  (_supply <100)){
-        return 40 ether;
+    else if ((_supply >= 50) &&  (_supply <70)){
+        return 45 ether;
     }
-    else if ((_supply >= 100) &&  (_supply <150)){
-        return 50 ether;
-    }
-    else if ((_supply >= 150) &&  (_supply <200)){
-        return 55 ether;
-    }
-    else if ((_supply >= 200) &&  (_supply <250)){
-        return 60 ether;
-    }
-    else if ((_supply >= 250) &&  (_supply <300)){
-        return 65 ether;
-    }
-    else if ((_supply >= 300) &&  (_supply <350)){
+    else if ((_supply >= 70) &&  (_supply <90)){
         return 70 ether;
     }
-    else if ((_supply >= 350) &&  (_supply <400)){
-        return 75 ether;
-    }
-    else if ((_supply >= 400) &&  (_supply <450)){
-        return 80 ether;
-    }
-    else if ((_supply >= 450) &&  (_supply <500)){
-        return 85 ether;
-    }
-    else if ((_supply >= 500) &&  (_supply <550)){
-        return 90 ether;
-    }
-    else if ((_supply >= 550) &&  (_supply <600)){
-        return 95 ether;
-    }
-    else if ((_supply >= 600) &&  (_supply <650)){
+    else if ((_supply >= 90) &&  (_supply <=100)){
         return 100 ether;
     }
-    else if ((_supply >= 650) &&  (_supply <700)){
-        return 105 ether;
-    }
-    else if ((_supply >= 700) &&  (_supply <750)){
-        return 110 ether;
-    }
-    else if ((_supply >= 750) &&  (_supply <800)){
-        return 115 ether;
-    }
-    else if ((_supply >= 800) &&  (_supply <850)){
-        return 120 ether;
-    }
-    else if ((_supply >= 850) &&  (_supply <900)){
-        return 125 ether;
-    }
-    else if ((_supply >= 900) &&  (_supply <950)){
-        return 130 ether;
-    }
-    else if ((_supply >= 950) &&  (_supply <1000)){
-        return 135 ether;
-    }
-    else if ((_supply >= 1000) &&  (_supply <1050)){
-        return 140 ether;
-    }
-    else if ((_supply >= 1050) &&  (_supply <1100)){
-        return 145 ether;
-    }
-    else if ((_supply >= 1100) &&  (_supply <1150)){
-        return 150 ether;
-    }
-    else if ((_supply >= 1150) &&  (_supply <1200)){
-        return 155 ether;
-    }
-    else if ((_supply >= 1200) &&  (_supply <1250)){
-        return 160 ether;
-    }
-    else if ((_supply >= 1250) &&  (_supply <1300)){
-        return 165 ether;
-    }
-    else if ((_supply >= 1300) &&  (_supply <1350)){
-        return 170 ether;
-    }
-    else if ((_supply >= 1350) &&  (_supply <1400)){
-        return 175 ether;
-    }
-    else if ((_supply >= 1400) &&  (_supply <1450)){
-        return 180 ether;
-    }
-    else if ((_supply >= 1450) &&  (_supply <1500)){
-        return 185 ether;
-    }
-    else if ((_supply >= 1500) &&  (_supply <1550)){
-        return 190 ether;
-    }
-    else if ((_supply >= 1550) &&  (_supply <1600)){
-        return 195 ether;
-    }
-    else if ((_supply >= 1600) &&  (_supply <1650)){
-        return 200 ether;
-    }
-    else if ((_supply >= 1650) &&  (_supply <1700)){
-        return 205 ether;
-    }
-    else if ((_supply >= 1700) &&  (_supply <1750)){
-        return 210 ether;
-    }
-    else if ((_supply >= 1750) &&  (_supply <1800)){
-        return 215 ether;
-    }
-    else if ((_supply >= 1800) &&  (_supply <1850)){
-        return 220 ether;
-    }
-    else if ((_supply >= 1850) &&  (_supply <1900)){
-        return 225 ether;
-    }
-    else if ((_supply >= 1900) &&  (_supply <1950)){
-        return 230 ether;
-    }
-    else if ((_supply >= 1950) &&  (_supply <2000)){
-        return 235 ether;
-    }
-    else if ((_supply >= 2000) &&  (_supply <2050)){
-        return 240 ether;
-    }
-    else if ((_supply >= 2050) &&  (_supply <2100)){
-        return 245 ether;
-    }
-    else if ((_supply >= 2100) &&  (_supply <2150)){
-        return 250 ether;
-    }
-    else if ((_supply >= 2150) &&  (_supply <2200)){
-        return 255 ether;
-    }
-    else if ((_supply >= 2200) &&  (_supply <2250)){
-        return 260 ether;
-    }
-    else if ((_supply >= 2250) &&  (_supply <2300)){
-        return 265 ether;
-    }
-    else if ((_supply >= 2300) &&  (_supply <2350)){
-        return 270 ether;
-    }
-    else if ((_supply >= 2350) &&  (_supply <2400)){
-        return 275 ether;
-    }
-    else if ((_supply >= 2400) &&  (_supply <2450)){
-        return 280 ether; 
-    }
-    else if ((_supply >= 2450) &&  (_supply <2500)){
-        return 285 ether;
-    }
-    else if ((_supply >= 2500) &&  (_supply <2550)){
-        return 290 ether;
-    }
-    else if ((_supply >= 2550) &&  (_supply <2600)){
-        return 295 ether;
-    }
-    else if ((_supply >= 2600) &&  (_supply <2650)){
-        return 300 ether;
-    }
-    else if ((_supply >= 2650) &&  (_supply <2700)){
-        return 305 ether;
-    }
-    else if ((_supply >= 2700) &&  (_supply <2750)){
-        return 310 ether;
-    }
-    else if ((_supply >= 2750) &&  (_supply <2800)){
-        return 315 ether;
-    }
-    else if ((_supply >= 2800) &&  (_supply <2851)){
-        return 320 ether;
-    }
+    
 
     else{
     return 10 ether;
@@ -1452,8 +1291,8 @@ function mint() public payable {
     
     if(supply > 0) {
         giftAddress = payable(ownerOf(randomNum(supply, block.timestamp, supply + 1) + 1));
-        // First % is for last mint and second % is for regular mint
-        giftValue = supply + 1 == maxSupply ? address(this).balance * 50 / 100 : msg.value * 50 / 100;
+
+        giftValue = supply + 1 == maxSupply ? address(this).balance * 10 / 100 : msg.value * 12 / 100;
     }
     
     _safeMint(msg.sender, supply + 1);
